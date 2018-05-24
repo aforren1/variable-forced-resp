313095c805bc5039f4c77b2c2f65937ba65a0960
Alex Forrence <aforren1@jhu.edu>
2018-05-24 18:28:28 -0400

    Few more exploratory things

--- analysis/practice.r
+++ analysis/practice.r
@@ -1,11 +1,17 @@
 library(ggplot2)
+library(ggridges)
 source('preprocess.r')
 source('misc.r')
 
 all_data <- preprocess()
 
 practice_data <- all_data[['practice_data']]
+practice_data <- practice_data[stim_type == 'symbol']
 
 ggplot(practice_data[stim_type == 'symbol'], aes(x=stim_val, y = real_prep_time)) + 
   geom_point() + 
   theme(axis.text.x=element_text(size=20))
+
+ggplot(practice_data[correct & real_prep_time < 1], 
+       aes(x = real_prep_time, y = stim_val, fill = stim_val)) + 
+  geom_density_ridges(alpha=0.6, scale=5, rel_min_height=0.03, from=0, to=1)
--- analysis/probe.r
+++ analysis/probe.r
@@ -8,8 +8,15 @@ probe_data <- all_data[['probe_data']]
 
 probe_data <- probe_data[stim_type == 'symbol']
 probe_data[, slide_correct := slider(x = real_prep_time, y = correct)]
-probe_data[, slide_correct_sym := slider(x = real_prep_time, y = correct), by = c('proposed_choice')]
+probe_data[, slide_stim := slider(x = real_prep_time, y = correct), by = c('stim_val', 'subject')]
+probe_data[, slide_finger := slider(x = real_prep_time, y = correct), by = c('proposed_choice', 'subject')]
 
-ggplot(probe_data, aes(x = real_prep_time, y = slide_correct_sym, colour = factor(proposed_choice))) + 
-  geom_line() + 
-  facet_wrap(~proposed_choice)
+
+
+ggplot(probe_data, aes(x = real_prep_time, y = slide_stim, colour = factor(proposed_choice))) + 
+  geom_line(size=1) + 
+  facet_wrap(stim_val~subject)
+
+ggplot(probe_data, aes(x = real_prep_time, y = slide_finger, colour = stim_val)) +
+  geom_line(size = 1) +
+  facet_wrap(proposed_choice ~ subject)
